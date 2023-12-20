b3fbac97adf35beb8f09fa294477c6f6d36ffb9c
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-20 10:39:28 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,5 @@
+---------------------------------------------------------------------
+Java Installation
+---------------------------------------------------------------------
+1. sudo apt install openjdk-11-jdk
+2. Java –version , to check java installation
